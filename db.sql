6e6216c8800eda65367009350eeb4e9baa552085
christoper__d <91582821+christoper-d@users.noreply.github.com>
2023-11-09 16:32:21 -0500

    Add files via upload

--- db.sql
+++ db.sql
@@ -1,34 +1,139 @@
-CREATE TABLE CONTACTO(
-IdContacto int primary key identity,
-Nombre varchar(40),
-Telefono varchar(40),
-FechaNacimiento date,
-FechaRegistro datetime default getdate()
-)
 
+CREATE TABLE users (
+    user_id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50) NOT NULL,
+    nombre VARCHAR(50),
+    ap_paterno VARCHAR(50),
+    ap_materno VARCHAR(50),
+    password VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE archivos (
+    archivo_id INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(100) NOT NULL,
+    archivo BLOB NOT NULL
+);
+
+CREATE TABLE permisos (
+    permiso_id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    archivo_id INT NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users (user_id),
+    FOREIGN KEY (archivo_id) REFERENCES archivos (archivo_id)
+);
+
+select * from users;
+insert into users values('admin','administrador','123');
+
+--para verificar el usuario
+go
+create or alter proc sp_login
+@user varchar(50),
+@pass varchar(50)
+as
+select * from users where username=@user and password1=@pass;
+go
+--para buscar usuario
+go
+create or alter proc sp_searchUser
+@user varchar(50)
+as
+select id from users where username=@user;
 go
 
-set dateformat dmy
-
-insert into CONTACTO(Nombre,Telefono,FechaNacimiento) values
-('Carlos','Masculino',CONVERT(date,'22/03/2005'))
-insert into CONTACTO(Nombre,Telefono,FechaNacimiento) values
-('Pepe','Masculino',CONVERT(date,'08/09/2009'))
-insert into CONTACTO(Nombre,Telefono,FechaNacimiento) values
-('Diego','Masculino',CONVERT(date,'14/10/2002'))
-insert into CONTACTO(Nombre,Telefono,FechaNacimiento) values
-('Camilo','Masculino',CONVERT(date,'11/10/2000'))
-insert into CONTACTO(Nombre,Telefono,FechaNacimiento) values
-('Jairo','Masculino',CONVERT(date,'08/06/2006'))
-
-insert into CONTACTO(Nombre,Telefono,FechaNacimiento) values
-('Alexa','Femenino',CONVERT(date,'17/04/2010'))
-insert into CONTACTO(Nombre,Telefono,FechaNacimiento) values
-('Fernanda','Femenino',CONVERT(date,'05/03/2001'))
-insert into CONTACTO(Nombre,Telefono,FechaNacimiento) values
-('Sharon','Femenino',CONVERT(date,'22/07/2003'))
-insert into CONTACTO(Nombre,Telefono,FechaNacimiento) values
-('Claudia','Femenino',CONVERT(date,'05/09/2004'))
+--para cambiar la contraseña
+go
+create or alter proc sp_changePassword
+@user varchar(50),
+@lastPassword varchar(50),
+@newPassword varchar(50)
+as
+begin
+	declare lastPassword varchar(50);
+	-- 
+	SELECT lastname INTO v_lastname
+	FROM employee
+	WHERE empno = p_empno;
+-- entonces aqui actualizamos el password del usuario
+update users set password1=@newPassword where username=@user and password1=@lastPassword;
+end
 go
 
-select * from CONTACTO
+select * from users;
+
+exec sp_changePassword 'admin','admin','123';
+
+exec sp_login 'admin','123';
+
+
+-------------------------------------------------------------------------------------------------------------------------------
+select * from users;
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50) NOT NULL,
+    nombre VARCHAR(50),
+    ap_paterno VARCHAR(50),
+    ap_materno VARCHAR(50),
+    password VARCHAR(100) NOT NULL
+);
+
+insert into users values('admin','administrador','123');
+
+--para verificar el usuario
+go
+create or alter proc sp_login
+@user varchar(50),
+@pass varchar(50)
+as
+select * from users where username=@user and password1=@pass;
+go
+--para buscar usuario
+go
+create or alter proc sp_searchUser
+@user varchar(50)
+as
+select id from users where username=@user;
+go
+
+--para cambiar la contraseña
+go
+create or alter proc sp_changePassword
+@user varchar(50),
+@lastPassword varchar(50),
+@newPassword varchar(50)
+as
+if(@user is null) or ( @lastPassword is null ) or ( @newPassword is null)
+	return 0
+else
+begin
+	update users set password1=@newPassword where username=@user and password1=@lastPassword;
+	return 1;
+
+-- entonces aqui actualizamos el password del usuario
+end;
+go
+
+select * from users;
+
+exec sp_changePassword 'admin','123','admin';
+
+exec sp_login 'admin','123';
+
+
+select * from users;
+go
+create or alter procedure sp_cambiar
+  @user varchar(40)=null,
+  @lastPassword varchar(50)=null,
+  @newPassword varchar(50)=null
+ as 
+ if (@user is null) or (@lastPassword is null) or (@newPassword is null)
+  select 'error';
+ else 
+ begin
+  update users set password1=@newPassword where username=@user and password1=@lastPassword;
+    select 'Cambio realizado';
+ end;
+ go
+
+ exec sp_cambiar 'chris','123','p123';
